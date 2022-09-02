64e08065d05f4f7f258e3fd5c7da77c8485c15b2
Nayuta <youmu331@gmail.com>
2022-09-03 01:47:08 +0900

    file_get_contentが使えなかったので変更

--- SpreadSheet.php
+++ SpreadSheet.php
@@ -20,12 +20,30 @@
     }
 
     private function getCSV(){
-      $csv=file_get_contents($this->url);
+      $csv=$this->GetURLContents($this->url);
       $csv_body = explode("\n",$csv);
       $result = [];
       foreach($csv_body as $row){
         $result[] = explode(',',$row);
       }
       return $result;
+    }
+
+    function GetURLContents($url){
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL, $url);
+      curl_setopt($ch, CURLOPT_HEADER, false);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+      //Locationをたどる
+      curl_setopt($ch,CURLOPT_FOLLOWLOCATION,true);
+      //最大何回リダイレクトをたどるか
+      curl_setopt($ch,CURLOPT_MAXREDIRS,10);
+      //リダイレクトの際にヘッダのRefererを自動的に追加させる
+      curl_setopt($ch,CURLOPT_AUTOREFERER,true);
+
+      $str = curl_exec($ch);
+      curl_close($ch);
+      return $str;
     }
   }
